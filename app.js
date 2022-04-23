dddaca1195fec2c8282e646d64f222864d51ec3b
Julie Combeau <combeau.julie@gmail.com>
2022-04-23 16:19:33 +0200

    quest 03 completed

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-// Import expres
+const connection = require("./db-config")
 const express = require("express");
-
-// Setup the environement variables form a .env file
-require("dotenv").config();
-
-// We store all express methods in a variable called app
 const app = express();
 
-// If an environment variable named PORT exists, we take it in order to let the user change the port without chaning the source code. Otherwise we give a default value of 3000
 const port = process.env.PORT ?? 3000;
 
-const connection = require("./db-config")
+require("dotenv").config();
 
 connection.connect((err) => {
   if (err) {
@@ -20,7 +14,9 @@ connection.connect((err) => {
   }
 });
 
-app.get('/api/movies', (req, res) => {
+app.use(express.json());
+
+app.get("/api/movies", (req, res) => {
   connection.query("SELECT * FROM movies", (err, result) => {
     if (err) {
       console.error(err);
@@ -29,7 +25,45 @@ app.get('/api/movies', (req, res) => {
       res.status(200).json(result);
     }
   })
-})
+});
+
+app.post("/api/movies", (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+connection.query("INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)", [title, director, year, color, duration],
+(err, result) => {
+  if (err) {
+    console.error(err);
+    res.status(500).send('Error saving the movie')
+  } else {
+    res.status(200).send('Movie successfully saved');
+  }
+}
+);
+});
+
+app.get("/api/users", (req, res) => {
+  connection.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database')
+    } else {
+      res.status(200).json(result);
+    }
+  })
+});
+
+app.post("/api/users", (req, res) => {
+  const { firstname, lastname, email } = req.body;
+  connection.query("INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)", [firstname, lastname, email],
+  (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving the movie')
+    } else {
+      res.status(200).send('Users successfully saved');
+    }
+  });
+});
 
 // app.get('/api/movies', (req, res) => {
 //   connection.promise().query("SELECT * FROM movies")

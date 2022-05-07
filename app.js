73622598f828412f8f98f7b7e6103aa61c931379
Julie Combeau <combeau.julie@gmail.com>
2022-05-07 12:21:06 +0200

    quest 8 completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const connection = require("./db-config")
 const express = require("express");
+const Joi = require('joi');
 const app = express();
 
 const port = process.env.PORT ?? 3000;
@@ -39,10 +40,12 @@ app.get("/api/movies", (req, res) => {
     sql += " WHERE duration <= ?";
     sqlValues.push(req.query.max_duration);
   }
-  if (req.query.color && req.query.max_duration){
-    sql += " WHERE color = ? AND duration <= ?"
-    sqlValues.push(req.query.color, req.query.max_duration)
+  if (req.query.color && req.query.max_duration) {
+    sql += " WHERE color = ?";
+    sqlValues.push(req.query.color);
+    sql += " AND duration <= ?";
   }
+  sqlValues.push(req.query.max_duration);
   connection.query(sql, sqlValues, (err, result) => {
     if (err) {
       console.error(err);
@@ -54,25 +57,53 @@ app.get("/api/movies", (req, res) => {
 });
 
 
-app.post("/api/movies", (req, res) => {
-  const { title, director, year, color, duration } = req.body;
-connection.query("INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)", [title, director, year, color, duration],
-(err, result) => {
-  if (err) {
-    console.error(err);
-    res.status(500).send('Error saving the movie')
-  } else {
-    const id = result.insertId;
-    const createdMovie = { id, title, director, year, color, duration};
-    res.status(201).json(createdMovie);
+app.post("/api/movies", (req, res) => {    
+const { title, director, year, color, duration } = req.body;
+
+    const { error } = Joi.object({
+      title: Joi.string().max(255).required(),
+      director: Joi.string().max(255).required(),
+      year: Joi.number().min(1888).required(),
+      color: Joi.boolean().required(),
+      duration: Joi.number().min(0),
+    }).validate({ title, director, year, color, duration }, { abortEarly: false });
+    
+    if (error) {
+      res.status(422).json({ validationErrors: error.details });
+    } else {
+      connection.query("INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)", [title, director, year, color, duration],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error saving the movie')
+        } else {
+          const id = result.insertId;
+          const createdMovie = { id, title, director, year, color, duration};
+          res.status(201).json(createdMovie);
+        }
+      }
+    );
   }
-}
-);
 });
 
 app.put("/api/movies/:movieId", (req, res) => {
   const { movieId } = req.params;
+  
+  const { title, director, year, color, duration } = req.body;
+
   const moviePropsToUpdate = req.body;
+
+  const { error } = Joi.object({
+  title: Joi.string().max(255),
+  director: Joi.string().max(255),
+  year: Joi.number().min(1888),
+  color: Joi.boolean(),
+  duration: Joi.number().min(0),
+}).validate({ title, director, year, color, duration }, { abortEarly: false });
+  
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
   connection.query(
     "UPDATE movies SET ? WHERE id = ?", [moviePropsToUpdate, movieId],
     (err, result) => {
@@ -86,7 +117,8 @@ app.put("/api/movies/:movieId", (req, res) => {
       }
     }
   );
- });
+}
+});
 
  app.delete("/api/movies/:id", (req,res) => {
   const movieId = req.params.id;
@@ -132,23 +164,65 @@ app.get("/api/users/:id", (req, res) => {
 });
 
 app.post("/api/users", (req, res) => {
-  const { firstname, lastname, email } = req.body;
-  connection.query("INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)", [firstname, lastname, email],
+  const { firstname, lastname, email, city, language } = req.body;
+
+  connection.query("SELECT * FROM users WHERE email = ?", [email],
+
   (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error saving the movie')
+    if (result[0]) {
+      res.status(409).json({ message: 'This email is already used' });
     } else {
-      const id = result.insertId;
-      const createdUser = { id, firstname, lastname, email};
-      res.status(201).json(createdUser);
+      const { error } = Joi.object({
+        email: Joi.string().email().max(255).required(),
+        firstname: Joi.string().max(255).required(),
+        lastname: Joi.string().max(255).required(),
+        city: Joi.string().max(255),
+        language: Joi.string().max(255),
+      }).validate({ firstname, lastname, email, city, language }, { abortEarly: false });
+      
+      if (error) {
+        res.status(422).json({ validationErrors: error.details });
+      } else {
+        connection.query("INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)", [firstname, lastname, email, city, language],
+        (err, result) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Error saving the movie')
+          } else {
+            const id = result.insertId;
+            const createdUser = { id, firstname, lastname, email, city, language};
+            res.status(201).json(createdUser);
+          }
+        }
+      );
     }
-  });
+  }
+})
 });
+
 
 app.put("/api/users/:userId", (req, res) => {
   const { userId } = req.params;
+  const { firstname, lastname, email, city, language } = req.body;
   const userPropsToUpdate = req.body;
+
+  connection.query("SELECT * FROM users WHERE email = ?", [email],
+
+  (err, result) => {
+    if (result[0]) {
+      res.status(409).json({ message: 'This email is already used' });
+    } else {
+      const { error } = Joi.object({
+        email: Joi.string().email().max(255),
+        firstname: Joi.string().max(255),
+        lastname: Joi.string().max(255),
+        city: Joi.string().max(255),
+        language: Joi.string().max(255),
+      }).validate({ firstname, lastname, email, city, language }, { abortEarly: false });
+      
+      if (error) {
+        res.status(422).json({ validationErrors: error.details });
+      } else {
   connection.query(
     "UPDATE users SET ? WHERE id = ?", [userPropsToUpdate, userId],
     (err, result) => {
@@ -160,8 +234,12 @@ app.put("/api/users/:userId", (req, res) => {
       } else {
         res.sendStatus(204);
       }
-    });
- });
+    }
+  );
+}
+}
+})
+});
 
  app.delete("/api/users/:id", (req,res) => {
   const userId = req.params.id;
